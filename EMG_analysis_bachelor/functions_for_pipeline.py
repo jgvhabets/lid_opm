cac9f7f8596c2d512ba3624f14ed5b529778eef0
lilianerheinlaender <lili.pink@yahoo.com>
2025-05-27 14:44:49 +0200

    Update functions_for_pipeline.py

--- EMG_analysis_bachelor/functions_for_pipeline.py
+++ EMG_analysis_bachelor/functions_for_pipeline.py
@@ -2,41 +2,126 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import mne
+from scipy.signal import butter, sosfiltfilt
 import matplotlib
 matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
 
 # here, I will write the functions that I can then use in the pipeline (to spare some space)
 
-def create_raw_df(data, column_names, times):
-    raw_df = pd.DataFrame(data.T, columns=column_names)
-    raw_df["Time (s)"] = times
-    return raw_df
+def create_df(data, column_names, times):
+    df = pd.DataFrame(data.T, columns=column_names)
+    df["Time (s)"] = times
+    return df
 
-def plot_raw_signals(columns, raw_df, location, emg):
-    fig, axs = plt.subplots(3, 3, figsize=(12, 8))
-    axs = axs.ravel()
-    for i, channel in enumerate(columns):
-        axs[i].plot(raw_df["Time (s)"], raw_df[channel])
-        axs[i].set_title(f"{channel} : signal of {location[channel]}")
-        axs[i].set_xlabel("Time (s)")
-        axs[i].set_ylabel("Amplitude (V)" if channel in emg else "g")
-
-        #if len(axs) > len(columns):
-        #    axs[-1].axis("off")
-
-    plt.tight_layout()
-    plt.show()
-
-def notch_filter(data, frequencies, sampling_rate):
-    notched_data = mne.filter.notch_filter(data, freqs=frequencies, Fs=sampling_rate)
-    return notched_data
+def notched(df, all_columns, times):
+    notched_df = df.copy()
+    for col in all_columns:
+        col_data = df[col].to_numpy()
+        notched_col = mne.filter.notch_filter(col_data, freqs=[50, 100, 150], Fs=1000)
+        notched_df[col] = notched_col
+    notched_df["Time (s)"] = times
+    return notched_df
 
 def bandpass_filter(data, sampling_rate, low_freq, high_freq):
     bandpassed_data = mne.filter.filter_data(data, sfreq=sampling_rate, l_freq=low_freq, h_freq=high_freq)
     return bandpassed_data
 
+def filtered(notched_dataframe, all_columns, acc_columns, emg_columns, times):
+    filtered_df = notched_dataframe.copy()
+    for col in all_columns:
+        if col in acc_columns:
+            col_data = notched_dataframe[col].to_numpy()
+            col_filtered = mne.filter.filter_data(col_data, sfreq=1000, l_freq=1, h_freq=48)
+            filtered_df[col] = col_filtered
+
+        if col in emg_columns:
+            col_data = notched_dataframe[col].to_numpy()
+            col_filtered = mne.filter.filter_data(col_data, sfreq=1000, l_freq=30, h_freq=480)
+            filtered_df[col] = col_filtered
+        else:
+            continue
+    filtered_df["Time (s)"] = times
+    return filtered_df # könnte das auch noch flexibler machen mit h_freq und l_freq indem ich ne liste als input tu und dann [0]und[1]!
+
 def create_filtered_df(data, columns, times):
     filtered_df = pd.DataFrame(data.T, columns=columns)
     filtered_df["Time (s)"] = times
     return filtered_df
+
+def plot_filtered_signals(channels, filtered_df, location, emg):
+    fig, axs = plt.subplots(3, 3, figsize=(12, 8))
+    axs = axs.ravel()
+    for i, channel in enumerate(channels):
+        axs[i].plot(filtered_df["Time (s)"], filtered_df[channel])
+        axs[i].set_title(f"{channel} : signal of {location[channel]}")
+        axs[i].set_xlabel("Time (s)")
+        axs[i].set_ylabel("Amplitude (V)" if channel in emg else "g")
+
+        #if len(axs) > len(channels):
+        #    axs[-1].axis("off")
+
+    plt.tight_layout()
+    plt.show()
+
+def plot_channel_overview(channels, df, title_name, location, emg): # only this function is needed right? does the same thing!! delete others?
+    fig, axs = plt.subplots(3, 3, figsize=(12, 8))
+    axs = axs.ravel()
+    for i, channel in enumerate(channels):
+        axs[i].plot(df["Time (s)"], df[channel])
+        axs[i].set_title(f"{channel} : signal of {location[channel]}")
+        axs[i].set_xlabel("Time (s)")
+        axs[i].set_ylabel("Amplitude (V)" if channel in emg else "g")
+        plt.suptitle(f"plots for {title_name}")
+
+        # if len(axs) > len(channels):
+        #    axs[-1].axis("off")
+
+    plt.tight_layout()
+    plt.show()
+
+
+def envelope(df, all_columns, emg_columns, times, freq):
+    emg_envelope_df = df.copy()
+    low_pass = freq/(1000/2)
+    sos = butter(4, low_pass, btype='lowpass', output="sos")
+    for col in all_columns:
+        if col in emg_columns:
+            col_data = df[col].to_numpy()
+            enveloped = sosfiltfilt(sos, x=col_data)
+            emg_envelope_df[col] = enveloped
+        else:
+            continue
+    emg_envelope_df["Time (s)"] = times
+    return emg_envelope_df
+
+
+def normalize_emg(df, all_columns, emg_columns, times):
+    normalized_emg_df = df.copy()
+    for col in all_columns:
+        if col in emg_columns:
+            col_data = df[col].to_numpy()
+            normalized_emg = col_data / np.max(np.abs(col_data))
+            normalized_emg_df[col] = normalized_emg
+            print(f"Normalized to range: [{np.min(normalized_emg): .2f}, {np.max(normalized_emg): .2f}]")
+        else:
+            continue
+    normalized_emg_df["Time (s)"] = times
+    return normalized_emg_df
+
+def movement_detection(data, threshold):
+    over_thresh = data >= threshold
+    return over_thresh
+
+def get_ch_indices(all_columns, emg, acc):
+    emg_cols = [all_columns.index(ch) for ch in emg if ch in all_columns]
+    acc_cols = [all_columns.index(ch) for ch in acc if ch in all_columns]
+    return emg_cols, acc_cols
+
+
+
+# df_raw = pd.DataFrame(.... [])
+#
+# df_filter = df_raw.copy()
+# df_filte[acc_cols] = df_raw[acc_cols].apply(filter_f, min_f = 10, max_f = 1000)
+# df_filter[meg_cols]
