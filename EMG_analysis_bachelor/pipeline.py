4ad23fe594625a0d4dd15af3e1557f0300903ecc
lilianerheinlaender <lili.pink@yahoo.com>
2025-07-05 11:25:12 +0200

    Update pipeline.py

--- EMG_analysis_bachelor/pipeline.py
+++ EMG_analysis_bachelor/pipeline.py
@@ -2,15 +2,12 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import mne
-import tkinter as tk
-from tkinter import *
-from tkinter import ttk
 import scipy as sp
 from matplotlib.lines import lineStyles
 from scipy.signal import butter, sosfiltfilt
 from scipy.stats import zscore
-from EMG_analysis_bachelor.functions_for_pipeline import get_ch_indices, plot_channel_overview, normalize_emg, notched, \
-    filtered, create_df, envelope
+from EMG_analysis_bachelor.functions_for_pipeline import get_ch_indices, plot_channel_overview, normalize_emg, \
+    notched_and_filtered, create_df, envelope, rectify
 import matplotlib
 matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
@@ -20,9 +17,14 @@ import matplotlib.pyplot as plt
  #                                  "PTB_measurement_14.04/Bonato_Federico_2025-04-14_13-02-56.cnt", preload=True)
 #EMG_only_test = mne.io.read_raw_ant("C:/Users/User/Documents/bachelorarbeit/data/EMG/"
  #                                  "test_first_2025-04-07_14-10-04_forearm_upperSide.cnt", preload=True)
-ACC_only_test = mne.io.read_raw_ant("C:/Users/User/Documents/bachelorarbeit/data/Accelerometer/"
-                                   "Test_Two_Charite_ACC_2025-06-10_11-53-04.cnt", preload=True)
-custom_order_names = ["BIP6", "BIP1", "BIP2", "BIP3", "BIP4", "BIP5", "BIP7", "BIP8"]
+#ACC_only_test = mne.io.read_raw_ant("C:/Users/User/Documents/bachelorarbeit/data/Accelerometer/"
+#                                   "Test_Two_Charite_ACC_2025-06-10_11-53-04.cnt", preload=True)
+leg_checks = mne.io.read_raw_ant("C:/Users/User/Documents/bachelorarbeit/data/EMG/Test_Leg_positions_synchron.cnt", preload=True)
+
+onsets = leg_checks.annotations.onset
+onsets = onsets * 1000
+custom_order_names = ["BIP11", "BIP12"]
+#custom_order_names = ["BIP6", "BIP1", "BIP2", "BIP3", "BIP4", "BIP5", "BIP7", "BIP8"]
 # custom_order_names = ["BIP7", "BIP8"]
 location = {"BIP7":"left forearm",
             "BIP8": "left delt",
@@ -31,14 +33,17 @@ location = {"BIP7":"left forearm",
             "BIP6" : "Charité ACC : x",
             "BIP3" : "2nd Charité ACC: x",
             "BIP4" : "2nd Charité ACC: y",
-            "BIP5" : "2nd Charité ACC: z"}
-EMG = ["BIP7", "BIP8"]
+            "BIP5" : "2nd Charité ACC: z",
+            "BIP11" : "right calf upper",
+            "BIP12" : "right calf lower"}
+#EMG = ["BIP7", "BIP8", "BIP11"]
+EMG = ["BIP11", "BIP12"]
 ACC = ["BIP6", "BIP1", "BIP2", "BIP3", "BIP4", "BIP5"]
 emg_idx, acc_idx = get_ch_indices(custom_order_names, EMG, ACC)
 
-data, times = ACC_only_test[custom_order_names, : ]
+data, times = leg_checks[custom_order_names, : ]
 data[0] *= -1
-data[4] *= -1
+# data[4] *= -1
 
 # creating raw df
 raw_df = create_df(data, custom_order_names, times)
@@ -46,17 +51,15 @@ raw_df = create_df(data, custom_order_names, times)
 #plotting the raw signals
 plot_channel_overview(custom_order_names, raw_df,"raw_signals", location, EMG)
 
-# apply notch filter
-notched_df = notched(raw_df, custom_order_names, times)
+# notch filter and bandpass filtering
+notched_and_filtered_df = notched_and_filtered(raw_df, custom_order_names, ACC, EMG, times)
 
-# filtering
-notched_and_filtered_df = filtered(notched_df, custom_order_names, ACC, EMG, times)
-
-# plot filtered data (overview?)
+# plot filtered data
 plot_channel_overview(custom_order_names, notched_and_filtered_df,"filtered_signals", location, EMG)
 
 # retrification and plotting
-rectified_df = abs(notched_and_filtered_df) #technically also took the absolute values of the time column, but that doesnt matter i think
+#rectified_df = abs(notched_and_filtered_df) #technically also took the absolute values of the time column, but that doesnt matter i think
+rectified_df = rectify(notched_and_filtered_df, custom_order_names, EMG)
 
 plot_channel_overview(custom_order_names, rectified_df, "rectified_signals", location, EMG)
 
@@ -67,8 +70,8 @@ emg_normalized_df = normalize_emg(rectified_df, custom_order_names, EMG, times) 
 plot_channel_overview(EMG, emg_normalized_df, "normalized_df", location, EMG)
 
 # build envelope
- #emg_envelopes = envelope(emg_normalized_df, custom_order_names, EMG, times, 3)
- ##plot_channel_overview(EMG, emg_envelopes, "normalized_df", location, EMG)
+emg_envelopes = envelope(emg_normalized_df, custom_order_names, EMG, times, 3)
+plot_channel_overview(EMG, emg_envelopes, "envelope_df", location, EMG)
  #
  ## movement detection
  #task_start_end = [1000, 15000] # pretending that that was trigger signal of start and end of task
