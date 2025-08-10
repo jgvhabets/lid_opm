f5c3f52219ab037eb576a0322056a0bd80377cc4
lilianerheinlaender <lili.pink@yahoo.com>
2025-08-10 16:53:19 +0200

    Update get_sub_dir.py

--- utils/get_sub_dir.py
+++ utils/get_sub_dir.py
@@ -5,25 +5,28 @@ from utils.find_paths import get_onedrive_path
 
 def get_sub_folder_dir(sub, data_subfolder: str, dType="emg_acc" ):
     """
-    used to get the directory of the sub folder
+    get the directory of the sub folder
     for getting or saving data to sub directory
     """
 
-    subfolder_dir = get_onedrive_path(data_subfolder)
+    subfolder_dir = get_onedrive_path("onedrive_charite", data_subfolder)
+    print(subfolder_dir)
 
     subfolder_options = ['project', 'figures', 'data',
                                 'raw_data', 'source_data',
-                                    'processed_data', 'results', ]
+                                    'processed_data', 'results',
+                                "classification_data", "onset_data"]
 
     if data_subfolder.lower() not in subfolder_options:
         raise ValueError(
             f'given folder: {data_subfolder} is incorrect, '
             f'should be {subfolder_options}')
 
-    if dType == 'emg_acc': folder = 'EMG_ACC_data'
+    if dType == 'emg_acc': folder = 'EMG_ACC'
 
     dir = os.path.join(subfolder_dir,
                             f'sub-{sub}',
                                 folder)
 
     return dir
+
