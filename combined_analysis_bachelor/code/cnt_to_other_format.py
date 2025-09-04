005e378e049b90c11c572eaa85888128f037a1ca
lilianerheinlaender <lili.pink@yahoo.com>
2025-09-04 16:53:26 +0200

    updated function for future recordings

--- combined_analysis_bachelor/code/cnt_to_other_format.py
+++ combined_analysis_bachelor/code/cnt_to_other_format.py
@@ -12,11 +12,39 @@ from scipy.signal import butter, sosfiltfilt
 from scipy.stats import zscore
 from combined_analysis_bachelor.code.functions_for_pipeline import get_ch_indices, plot_channel_overview, normalize_emg, \
 create_df
-from utils.get_sub_dir import get_sub_folder_dir
+from my_utils.get_sub_dir import get_sub_folder_dir
 import matplotlib
 matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
 
+
+setupA_dict = {"BIP11": "brachioradialis_R",
+          "BIP8": "deltoideus_R",
+          "BIP9": "brachioradialis_L",
+          "BIP10": "deltoideus_L",
+          "BIP7": "tibialisAnterior_R",
+          "BIP12": "tibialisAnterior_L",
+          "BIP1": "acc_y_hand_R",
+          "BIP2": "acc_z_hand_R",
+          "BIP6": "acc_x_hand_R",
+          "BIP3": "acc_x_hand_L",
+          "BIP4": "acc_y_hand_L",
+          "BIP5": "acc_z_hand_L",
+}
+
+setupB_dict = {"BIP11": "brachioradialis_R",
+          "BIP8": "deltoideus_R",
+          "BIP9": "brachioradialis_L",
+          "BIP10": "deltoideus_L",
+          "BIP7": "tibialisAnterior_R",
+          "BIP12": "tibialisAnterior_L",
+          "BIP1": "acc_y_leg_L",
+          "BIP2": "acc_z_leg_L",
+          "BIP6": "acc_x_leg_L",
+          "BIP3": "acc_x_hand_L",
+          "BIP4": "acc_y_hand_L",
+          "BIP5": "acc_z_hand_L",
+}
 
 def cnt_to_raw_hdf(sub, source_directory, target_directory, channels, emg_channels, location_dict, sampling_freq):
     filepaths = []
@@ -27,7 +55,7 @@ def cnt_to_raw_hdf(sub, source_directory, target_directory, channels, emg_channe
 
     print(f"retrieved data: {filepaths}")
 
-    # ---- initializing json file ---- #
+    # ---- initializing json file ---- #  --> saving rec durations needed for synchronization with OPM data
     json_path = f"{target_directory}\\sub-{sub}_recording_durations.json"
     print(json_path)
 
@@ -64,25 +92,28 @@ def cnt_to_raw_hdf(sub, source_directory, target_directory, channels, emg_channe
             onsets = source.annotations.onset
             print(onsets)
             onsets_samples = onsets * sampling_freq
-            start_trig = int(onsets_samples[0])
-            stop_trig = int(onsets_samples[-1])
+            #start_trig = int(onsets_samples[0]) # this should be used for future recordings where trigger marks start and end
+            #stop_trig = int(onsets_samples[-1])
 
 
-            #if filepath == filepaths[3]: # in Rec 3 (but in new order=Rec4) there were random trig. signals too early --> how can i make this modualar?
-            #    sync_trig = onsets[3]
-            #    sync_trig_samples = int(onsets_samples[3])
+            if filepath == filepaths[3]: # in Rec 3 (but in new order=Rec4) there were random trig. signals too early --> how can i make this modualar?
+                sync_trig = onsets[3]
+                sync_trig_samples = int(onsets_samples[3])
 
-            #else:
-            #    sync_trig = onsets[0] # in seconds
-            #    sync_trig_samples = int(onsets_samples[0]) # in samples
+            else:
+                sync_trig = onsets[0] # in seconds
+                sync_trig_samples = int(onsets_samples[0]) # in samples
 
 
             # ----------- trimming ----------- #
-            data_trimmed = data[:, start_trig:stop_trig]
-            source_times = times[start_trig:stop_trig]
+            #data_trimmed = data[:, start_trig:stop_trig] # this should be used for future recordings
+            #source_times = times[start_trig:stop_trig]
+            data_trimmed = data[:, sync_trig:]
+            source_times = times[sync_trig:]
 
             # ---------- getting sync_times --------- #
-            sync_times = source_times - (start_trig/1000)
+            #sync_times = source_times - (start_trig/1000)
+            sync_times = source_times - (sync_trig / 1000)
 
 
             # ---------- creating dataframe ---------- #
@@ -113,80 +144,10 @@ def cnt_to_raw_hdf(sub, source_directory, target_directory, channels, emg_channe
             print("Datei existiert:", os.path.exists(target_filepath))
 
             # --- saving to json file --- #
-            durations[filename[:-7]] = sync_times[-1]
+            durations[filename[:-7]] = sync_times[-1] # saving durations for synchronizations with OPM data
 
             with open(json_path, 'w') as f:
                 json.dump(durations, f, indent=4)
 
     print("\n✅ Every File and .JSON processed and saved!")
 
-#cnt_to_raw_hdf(SUB, source_dir, target_dir, channels_c, EMG, LOCS, SF)
-
-#def cnt_to_raw_hdf_safe(sub, source_directory, target_directory, channels, location_dict, sampling_freq):
-#    filepaths = []
-#    for file in os.listdir(source_directory):
-#        if file.endswith(".cnt"):
-#            filepath = f"{source_directory}\\{file}"
-#            filepaths.append(filepath)
-#
-#    print(f"retrieved data: {filepaths}")
-#
-#    # ========= looping trough filedirectory ======== #
-#    for filepath in filepaths:
-#        # --- read in file --- #
-#        if os.path.basename(filepath).startswith(f"sub_{sub}"):
-#            source = mne.io.read_raw_ant(filepath, preload=True)
-#            data, times = source[channels, :]
-#
-#
-#        # --- correcting inverted channels --- #
-#            data[channels.index("BIP4")] *= -1
-#            #data[1] *= -1
-#            #data[6] *= -1
-#
-#
-#            # --- finding trigger --- #
-#            onsets = source.annotations.onset
-#            onsets_samples = onsets * sampling_freq
-#            onsets_df = pd.DataFrame(onsets_samples)
-#
-#            print(onsets)
-#
-#            sync_trig = onsets[0]
-#            sync_trig_samples = int(sync_trig * sampling_freq)
-#            print(sync_trig_samples)
-#
-#
-#
-#            # ----------- trimming ----------- #
-#            data_trimmed = data[:, sync_trig_samples:]
-#            source_times = times[sync_trig_samples:]
-#
-#            # ---------- getting sync_times --------- #
-#            sync_times = source_times - sync_trig
-#
-#            # ---------- creating dataframe ---------- #
-#            data_trimmed_T = data_trimmed.T
-#            raw_df = pd.DataFrame(data_trimmed_T, columns=[location_dict[ch] for ch in channels])
-#            assert all(ch in location_dict for ch in channels), "Some channels missing in your location_dict!"
-#
-#            raw_df["Sync_Time (s)"] = sync_times
-#            raw_df["Source_Time (s)"] = source_times
-#
-#            # --------- creating new file -------- #
-#            filename = os.path.basename(filepath)
-#            if filename.startswith(f"sub_{sub}") and filename.endswith(".cnt"):
-#                filename = filename.replace(f"sub_{sub}", f"sub-{sub}")
-#                print(filename)
-#            filename += "_raw.h5"
-#
-#            target_filepath = os.path.join(target_directory, filename)
-#
-#            print(f"Saving to: {target_filepath}")
-#
-#            raw_df.to_hdf(target_filepath, key="data", mode="w")
-#
-#    print("\n✅ Every File processed and saved!")
-#
-#
-#
